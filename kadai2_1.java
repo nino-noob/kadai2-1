13e20eb9e1c47cadb84715efe81752fe58332851
nino-noob <g22929ny@gm.tsuda.ac.jp>
2025-04-25 17:03:00 +0900

    Add files via upload

---
+++ kadai2_1.java
@@ -0,0 +1,9 @@
+public class kadai2_1 {
+    public static void main(String[] args) {
+        String food = "あんこ";
+        String reason = "大好きな和菓子には欠かせないから！";
+        
+        System.out.println("私の好きな食べ物は " + food + "です!");
+        System.out.println("なぜなら、" + reason);
+    }
+}
