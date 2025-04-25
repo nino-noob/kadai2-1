fa4850d4f604799526ba65e83e4a7e7d6d0d0566
nino-noob <g22929ny@gm.tsuda.ac.jp>
2025-04-25 17:22:31 +0900

    Add files via upload

---
+++ kadai2_2.java
@@ -0,0 +1,15 @@
+public class kadai2_2 {
+    public static void main(String[] args) {
+        String food = "あんこ";
+        String reason = "大好きな和菓子には欠かせないから！";
+        
+        System.out.println("私の好きな食べ物は " + food + "です!");
+        System.out.println("なぜなら、" + reason);
+
+        String favoriteplace="自分の部屋";
+        String reason2="ゲームを好きなだけできるから!";
+
+        System.out.println("私の好きな場所は"+favoriteplace+"です。");
+        System.out.println("なぜなら、"+reason2);
+    }
+}
