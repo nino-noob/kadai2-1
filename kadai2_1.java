367b6574451bf301495bfce1d7ca8098f2030ca8
nino-noob <g22929ny@gm.tsuda.ac.jp>
2025-04-26 16:41:01 +0900

    Update kadai2_1.java

--- kadai2_1.java
+++ kadai2_1.java
@@ -2,7 +2,9 @@ public class kadai2_1 {
     public static void main(String[] args) {
         String food = "あんこ";
         String reason = "大好きな和菓子には欠かせないから！";
-        
+
+        String place="イタリア";
+        String reason="食べ物全部が美味しいから！";
         System.out.println("私の好きな食べ物は " + food + "です!");
         System.out.println("なぜなら、" + reason);
     }
